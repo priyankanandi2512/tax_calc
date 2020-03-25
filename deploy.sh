08ac1be085efcba69ee1bfa957d1f9838461be21
Priyanka Nandi <priyanka.nandi@infobip.com>
2020-03-25 20:34:57 +0530

    remove unwanted

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-chmod +x .env
-set -a
-source .env
-set +a && npm install && npm run build && cd terraform && ~/aws/terraform apply && ~/aws/terraform output config > ../src/app-config.json
+# #!/bin/bash
+# chmod +x .env
+# set -a
+# source .env
+# set +a && npm install && npm run build && cd terraform && ~/aws/terraform apply && ~/aws/terraform output config > ../src/app-config.json
